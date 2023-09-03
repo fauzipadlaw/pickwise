b8f1d50c9ea293a7ffeddf85e6983b862eb1a4ec
Muhammad Fauzi <fauzi.padlaw@gmail.com>
2023-09-03 15:08:28 +0700

    await decisionProvider

--- lib/providers/data.provider.dart
+++ lib/providers/data.provider.dart
@@ -2,4 +2,4 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive/hive.dart';
 
 final decicionsProvider =
-    StateProvider((ref) => Hive.openBox<Map>('decisions'));
+    StateProvider((ref) async => await Hive.openBox<Map>('decisions'));
